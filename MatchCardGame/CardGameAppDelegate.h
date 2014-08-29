71c6cfaf7e8e0611a7d3c032fd477c667824d685
Cyro Guimaraes <cyro.guimaraes@corp.globo.com>
2014-08-29 08:25:55 -0300

    Initial Commit

---
+++ MatchCardGame/CardGameAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  CardGameAppDelegate.h
+//  MatchCardGame
+//
+//  Created by Cyro Guimaraes on 8/29/14.
+//  Copyright (c) 2014 MAB604. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CardGameAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ MatchCardGame/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  MatchCardGame
+//
+//  Created by Cyro Guimaraes on 8/29/14.
+//  Copyright (c) 2014 MAB604. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "CardGameAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([CardGameAppDelegate class]));
+    }
+}
