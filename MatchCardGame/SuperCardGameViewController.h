65a5baff4b1938500c1235dfce80960032b5f4de
cyro809 <cyro.guimaraes@corp.globo.com>
2014-10-19 12:31:39 -0200

    Alterando nome das imagens

---
+++ MatchCardGame/SuperCardGameViewController.h
@@ -0,0 +1,13 @@
+//
+//  SuperCardGameViewController.h
+//  MatchCardGame
+//
+//  Created by Cyro Guimaraes on 10/18/14.
+//  Copyright (c) 2014 MAB604. All rights reserved.
+//
+
+#import "CardGameViewController.h"
+
+@interface SuperCardGameViewController : CardGameViewController
+
+@end
---
+++ MatchCardGame/SuperCardGameViewController.m
@@ -0,0 +1,49 @@
+//
+//  SuperCardGameViewController.m
+//  MatchCardGame
+//
+//  Created by Cyro Guimaraes on 10/18/14.
+//  Copyright (c) 2014 MAB604. All rights reserved.
+//
+
+#import "SuperCardGameViewController.h"
+
+@interface SuperCardGameViewController ()
+
+@end
+
+@implementation SuperCardGameViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
